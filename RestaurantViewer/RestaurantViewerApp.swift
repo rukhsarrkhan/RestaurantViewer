9e3b562aa0d90fee7c38d63cd77ee24b452b1bac
rukhsarrkhan <rukhsarkhan4198@gmail.com>
2024-01-27 16:46:29 -0500

    Initial Commit

---
+++ RestaurantViewer/RestaurantViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantViewerApp.swift
+//  RestaurantViewer
+//
+//  Created by Rukhsar Khan on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
